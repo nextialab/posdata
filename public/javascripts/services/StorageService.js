4170648414100494c6b91586a7f07a9ba4dec963
Nelson Iván González <nigm2005@gmail.com>
2016-09-28 16:00:13 -0500

    Returning to localStorage, defaults to cookies if localStorage is not supported

--- public/javascripts/services/StorageService.js
+++ public/javascripts/services/StorageService.js
@@ -1,16 +1,36 @@
 'use strict';
 angular.module('services').factory('StorageService', ['$cookies', function ($cookies) {
-    return {
-        getItem: function (name) {
-            var value = $cookies.get(name);
-            if (value !== undefined) {
-                return value;
-            } else {
-                return '';
+    var storageProvider;
+    if (typeof(Storage) !== 'undefined') { // use local storage
+        console.log('Using local storage');
+        storageProvider = {
+            getItem: function (name) {
+                var value = localStorage.getItem(name);
+                if (value !== null) {
+                    return value;
+                } else {
+                    return '';
+                }
+            },
+            setItem: function (name, value) {
+                localStorage.setItem(name, value);
             }
-        },
-        setItem: function (name, value) {
-            $cookies.put(name, value);
         }
-    };
+    } else { // default to cookies
+        console.log('Defaulting to cookies');
+        storageProvider = {
+            getItem: function (name) {
+                var value = $cookies.get(name);
+                if (value !== undefined) {
+                    return value;
+                } else {
+                    return '';
+                }
+            },
+            setItem: function (name, value) {
+                $cookies.put(name, value);
+            }
+        }
+    }
+    return storageProvider;
 }]);
